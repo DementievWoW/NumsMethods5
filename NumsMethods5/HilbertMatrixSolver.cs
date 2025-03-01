6b7253fa62d479fed3c64d6a376a5c2356fc3b9e
DementievWoW <dementievwow@mail.ru>
2025-03-01 19:03:17 +0300

    final

---
+++ NumsMethods5/HilbertMatrixSolver.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumsMethods5
+{
+    internal static class HilbertMatrixSolver
+    {
+        public static double[,] GilbertMatrix(int size = 8)
+        {
+            double[,] matrix = new double[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = 1.0 / (i + j + 1);
+                }
+            }
+            return matrix;
+        }
+
+        public static double[] Gauss(double[,] A, double[] b)
+        {
+            int size = A.GetLength(0);
+            double[,] A_copy = new double[size, size];
+            double[] b_copy = new double[size];
+
+            // Копирование массивов, чтобы не изменять исходные данные
+            Array.Copy(b, b_copy, size);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    A_copy[i, j] = A[i, j];
+                }
+            }
+
+
+            for (int i = 0; i < size; i++)
+            {
+                double pivot = A_copy[i, i];
+                for (int j = 0; j < size; j++)
+                {
+                    A_copy[i, j] /= pivot;
+                }
+                b_copy[i] /= pivot;
+
+                for (int j = i + 1; j < size; j++)
+                {
+                    double factor = A_copy[j, i];
+                    for (int k = 0; k < size; k++)
+                    {
+                        A_copy[j, k] -= factor * A_copy[i, k];
+                    }
+                    b_copy[j] -= factor * b_copy[i];
+                }
+            }
+
+            double[] x = new double[size];
+            for (int i = size - 1; i >= 0; i--)
+            {
+                double sum = 0;
+                for (int j = i + 1; j < size; j++)
+                {
+                    sum += A_copy[i, j] * x[j];
+                }
+                x[i] = b_copy[i] - sum;
+            }
+
+            return x;
+        }
+
+        public static double[] MatrixVectorMultiply(double[,] A, double[] x)
+        {
+            int rows = A.GetLength(0);
+            int cols = A.GetLength(1);
+            if (cols != x.Length)
+            {
+                throw new ArgumentException("Размеры матрицы и вектора не совпадают");
+            }
+
+            double[] result = new double[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < cols; j++)
+                {
+                    sum += A[i, j] * x[j];
+                }
+                result[i] = sum;
+            }
+            return result;
+        }
+
+        public static double VectorNorm(double[] vector)
+        {
+            double sumOfSquares = 0;
+            foreach (double val in vector)
+            {
+                sumOfSquares += val * val;
+            }
+            return Math.Sqrt(sumOfSquares);
+        }
+
+        public static double[,] MatrixTranspose(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            double[,] transposedMatrix = new double[cols, rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    transposedMatrix[j, i] = matrix[i, j];
+                }
+            }
+            return transposedMatrix;
+        }
+
+        public static double[,] MatrixMatrixMultiply(double[,] matrixA, double[,] matrixB)
+        {
+            int rowsA = matrixA.GetLength(0);
+            int colsA = matrixA.GetLength(1);
+            int rowsB = matrixB.GetLength(0);
+            int colsB = matrixB.GetLength(1);
+
+            if (colsA != rowsB)
+            {
+                throw new ArgumentException("Недопустимые размеры матриц для умножения.");
+            }
+
+            double[,] result = new double[rowsA, colsB];
+
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsB; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < colsA; k++)
+                    {
+                        sum += matrixA[i, k] * matrixB[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+
+            return result;
+        }
+
+        public static double[,] MatrixAdd(double[,] matrixA, double[,] matrixB)
+        {
+            int rowsA = matrixA.GetLength(0);
+            int colsA = matrixA.GetLength(1);
+            int rowsB = matrixB.GetLength(0);
+            int colsB = matrixB.GetLength(1);
+
+            if (rowsA != rowsB || colsA != colsB)
+            {
+                throw new ArgumentException("Матрицы должны иметь одинаковые размеры для сложения.");
+            }
+
+            double[,] result = new double[rowsA, colsA];
+            for (int i = 0; i < rowsA; i++)
+            {
+                for (int j = 0; j < colsA; j++)
+                {
+                    result[i, j] = matrixA[i, j] + matrixB[i, j];
+                }
+            }
+            return result;
+        }
+    }
+}
--- NumsMethods5/Program.cs
+++ NumsMethods5/Program.cs
@@ -1,2 +1,128 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static NumsMethods5.HilbertMatrixSolver;
+int size = 8;
+
+double[,] A = GilbertMatrix(size);
+Console.WriteLine("Матрица Гильберта:");
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Console.Write(A[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+double[] x_star = Enumerable.Range(1, size).Select(x => (double)x).ToArray();
+double[] b = MatrixVectorMultiply(A, x_star);
+Console.WriteLine("\nВектор b:");
+foreach (double val in b)
+{
+    Console.WriteLine(val + " ");
+}
+Console.WriteLine();
+
+// Формирование возмущённой правой части b̃, таким образом, чтобы случайные
+// величины вектора возмущения δb = b − b̃ были по модулю порядка 10−7 – 10−8
+
+Random rand = new Random();
+double[] delta_b = new double[size];
+for (int i = 0; i < size; i++)
+{
+    // Генерация случайного числа в диапазоне 10^-8 до 10^-6
+    double rangeMin = Math.Pow(10, -8);
+    double rangeMax = Math.Pow(10, -6);
+    delta_b[i] = rangeMin + (rangeMax - rangeMin) * rand.NextDouble();
+    delta_b[i] *= (rand.Next(2) == 0 ? -1 : 1); // Случайный знак
+}
+
+
+double[] b_tilde = new double[size];
+for (int i = 0; i < size; i++)
+{
+    b_tilde[i] = b[i] - delta_b[i];
+}
+
+
+// Абсолютная и относительная нормы возмущения
+double abs_norm_b = VectorNorm(delta_b);
+double rel_norm_b = abs_norm_b / VectorNorm(b);
+Console.WriteLine("Абсолютная норма возмущения правой части: " + abs_norm_b);
+Console.WriteLine("Относительная норма возмущения правой части: " + rel_norm_b);
+
+// Найдем решение возмущённой системы Ax̃ = b̃, невязку для найденного решения
+// r = Ax̃ − b̃ и норму невязки ∥r∥
+double[] x_tilde = Gauss(A, b_tilde); // Использовать свой Gauss, а не MathNet.Numerics
+
+double[] r = MatrixVectorMultiply(A, x_tilde);
+for (int i = 0; i < size; i++)
+{
+    r[i] -= b_tilde[i];
+}
+
+
+double r_norm = VectorNorm(r);
+Console.WriteLine("Невязка для решения СЛАУ r = Ax - b: ");
+foreach (double val in r)
+{
+    Console.WriteLine(val + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Норма невязки ||r||: " + r_norm);
+
+//возмущение решения δx = x − x̃, абсолютную и относительную нормы возмущения решения
+double[] delta_x = new double[size];
+for (int i = 0; i < size; i++)
+{
+    delta_x[i] = x_star[i] - x_tilde[i];
+}
+
+double abs_norm_x = VectorNorm(delta_x);
+double rel_norm_x = abs_norm_x / VectorNorm(x_star);
+Console.WriteLine("Абсолютная норма возмущения решения: " + abs_norm_x);
+Console.WriteLine("Относительная норма возмущения решения: " + rel_norm_x);
+
+Console.WriteLine("eps_x / eps_b = " + rel_norm_x / rel_norm_b);
+
+
+double lambda_reg = 0.0000000000001;
+
+// Создание единичной матрицы
+double[,] identity = new double[size, size];
+for (int i = 0; i < size; i++)
+{
+    identity[i, i] = 1.0;
+}
+
+double[,] AT = MatrixTranspose(A);
+double[,] ATA = MatrixMatrixMultiply(AT, A);
+double[,] lambdaI = new double[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    lambdaI[i, i] = lambda_reg;
+}
+
+double[,] leftSide = MatrixAdd(ATA, lambdaI);
+
+double[] rightSide = MatrixVectorMultiply(AT, b);
+
+double[] result = Gauss(leftSide, rightSide);
+
+Console.WriteLine("Решение регуляризацией: ");
+foreach (double val in result)
+{
+    Console.WriteLine(val + " ");
+}
+Console.WriteLine();
+
+double totalDeviationExact = 0;
+for (int i = 0; i < size; i++)
+{
+    totalDeviationExact += Math.Abs(result[i] - x_star[i]);
+}
+Console.WriteLine("Отклонение от точного решения: " + totalDeviationExact);
+
+
